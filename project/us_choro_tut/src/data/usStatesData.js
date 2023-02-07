d2edf75ecebc2b142dd304eac4d2deacb8b07a7a
Zach1281 <zachary_romero@yahoo.com>
2023-02-07 10:01:37 -0800

    project tutorial upload

---
+++ project/us_choro_tut/src/data/usStatesData.js
@@ -0,0 +1,257 @@
+export var pyDepartmentsData = [
+  {
+    Geographic_Area: "Alabama",
+    Postal_Code: "AL",
+    Total_Resident_Population: 4903185
+  },
+  {
+    Geographic_Area: "Alaska",
+    Postal_Code: "AK",
+    Total_Resident_Population: 731545
+  },
+  {
+    Geographic_Area: "Arizona",
+    Postal_Code: "AZ",
+    Total_Resident_Population: 7278717
+  },
+  {
+    Geographic_Area: "Arkansas",
+    Postal_Code: "AR",
+    Total_Resident_Population: 3017804
+  },
+  {
+    Geographic_Area: "California",
+    Postal_Code: "CA",
+    Total_Resident_Population: 39512223
+  },
+  {
+    Geographic_Area: "Colorado",
+    Postal_Code: "CO",
+    Total_Resident_Population: 5758736
+  },
+  {
+    Geographic_Area: "Connecticut",
+    Postal_Code: "CT",
+    Total_Resident_Population: 3565287
+  },
+  {
+    Geographic_Area: "Delaware",
+    Postal_Code: "DE",
+    Total_Resident_Population: 973764
+  },
+  {
+    Geographic_Area: "District of Columbia",
+    Postal_Code: "DC",
+    Total_Resident_Population: 705749
+  },
+  {
+    Geographic_Area: "Florida",
+    Postal_Code: "FL",
+    Total_Resident_Population: 21477737
+  },
+  {
+    Geographic_Area: "Georgia",
+    Postal_Code: "GA",
+    Total_Resident_Population: 10617423
+  },
+  {
+    Geographic_Area: "Hawaii",
+    Postal_Code: "HI",
+    Total_Resident_Population: 1415872
+  },
+  {
+    Geographic_Area: "Idaho",
+    Postal_Code: "ID",
+    Total_Resident_Population: 1787065
+  },
+  {
+    Geographic_Area: "Illinois",
+    Postal_Code: "IL",
+    Total_Resident_Population: 12671821
+  },
+  {
+    Geographic_Area: "Indiana",
+    Postal_Code: "IN",
+    Total_Resident_Population: 6732219
+  },
+  {
+    Geographic_Area: "Iowa",
+    Postal_Code: "IA",
+    Total_Resident_Population: 3155070
+  },
+  {
+    Geographic_Area: "Kansas",
+    Postal_Code: "KS",
+    Total_Resident_Population: 2913314
+  },
+  {
+    Geographic_Area: "Kentucky",
+    Postal_Code: "KY",
+    Total_Resident_Population: 4467673
+  },
+  {
+    Geographic_Area: "Louisiana",
+    Postal_Code: "LA",
+    Total_Resident_Population: 4648794
+  },
+  {
+    Geographic_Area: "Maine",
+    Postal_Code: "ME",
+    Total_Resident_Population: 1344212
+  },
+  {
+    Geographic_Area: "Maryland",
+    Postal_Code: "MD",
+    Total_Resident_Population: 6045680
+  },
+  {
+    Geographic_Area: "Massachusetts",
+    Postal_Code: "MA",
+    Total_Resident_Population: 6892503
+  },
+  {
+    Geographic_Area: "Michigan",
+    Postal_Code: "MI",
+    Total_Resident_Population: 9986857
+  },
+  {
+    Geographic_Area: "Minnesota",
+    Postal_Code: "MN",
+    Total_Resident_Population: 5639632
+  },
+  {
+    Geographic_Area: "Mississippi",
+    Postal_Code: "MS",
+    Total_Resident_Population: 2976149
+  },
+  {
+    Geographic_Area: "Missouri",
+    Postal_Code: "MO",
+    Total_Resident_Population: 6137428
+  },
+  {
+    Geographic_Area: "Montana",
+    Postal_Code: "MT",
+    Total_Resident_Population: 1068778
+  },
+  {
+    Geographic_Area: "Nebraska",
+    Postal_Code: "NE",
+    Total_Resident_Population: 1934408
+  },
+  {
+    Geographic_Area: "Nevada",
+    Postal_Code: "NV",
+    Total_Resident_Population: 3080156
+  },
+  {
+    Geographic_Area: "New Hampshire",
+    Postal_Code: "NH",
+    Total_Resident_Population: 1359711
+  },
+  {
+    Geographic_Area: "New Jersey",
+    Postal_Code: "NJ",
+    Total_Resident_Population: 8882190
+  },
+  {
+    Geographic_Area: "New Mexico",
+    Postal_Code: "NM",
+    Total_Resident_Population: 2096829
+  },
+  {
+    Geographic_Area: "New York",
+    Postal_Code: "NY",
+    Total_Resident_Population: 19453561
+  },
+  {
+    Geographic_Area: "North Carolina",
+    Postal_Code: "NC",
+    Total_Resident_Population: 10488084
+  },
+  {
+    Geographic_Area: "North Dakota",
+    Postal_Code: "ND",
+    Total_Resident_Population: 762062
+  },
+  {
+    Geographic_Area: "Ohio",
+    Postal_Code: "OH",
+    Total_Resident_Population: 11689100
+  },
+  {
+    Geographic_Area: "Oklahoma",
+    Postal_Code: "OK",
+    Total_Resident_Population: 3956971
+  },
+  {
+    Geographic_Area: "Oregon",
+    Postal_Code: "OR",
+    Total_Resident_Population: 4217737
+  },
+  {
+    Geographic_Area: "Pennsylvania",
+    Postal_Code: "PA",
+    Total_Resident_Population: 12801989
+  },
+  {
+    Geographic_Area: "Rhode Island",
+    Postal_Code: "RI",
+    Total_Resident_Population: 1059361
+  },
+  {
+    Geographic_Area: "South Carolina",
+    Postal_Code: "SC",
+    Total_Resident_Population: 5148714
+  },
+  {
+    Geographic_Area: "South Dakota",
+    Postal_Code: "SD",
+    Total_Resident_Population: 884659
+  },
+  {
+    Geographic_Area: "Tennessee",
+    Postal_Code: "TN",
+    Total_Resident_Population: 6829174
+  },
+  {
+    Geographic_Area: "Texas",
+    Postal_Code: "TX",
+    Total_Resident_Population: 28995881
+  },
+  {
+    Geographic_Area: "Utah",
+    Postal_Code: "UT",
+    Total_Resident_Population: 3205958
+  },
+  {
+    Geographic_Area: "Vermont",
+    Postal_Code: "VT",
+    Total_Resident_Population: 623989
+  },
+  {
+    Geographic_Area: "Virginia",
+    Postal_Code: "VA",
+    Total_Resident_Population: 8535519
+  },
+  {
+    Geographic_Area: "Washington",
+    Postal_Code: "WA",
+    Total_Resident_Population: 7614893
+  },
+  {
+    Geographic_Area: "West Virginia",
+    Postal_Code: "WV",
+    Total_Resident_Population: 1792147
+  },
+  {
+    Geographic_Area: "Wisconsin",
+    Postal_Code: "WI",
+    Total_Resident_Population: 5822434
+  },
+  {
+    Geographic_Area: "Wyoming",
+    Postal_Code: "WY",
+    Total_Resident_Population: 578759
+  }
+]
